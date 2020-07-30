80f7c88d02b1f1c347a926b7cdc5785fd3a1ef3f
bcm32 <bcm3223@gmail.com>
2020-07-30 14:01:09 -0400

    clearsave "fix"

--- src/settings/Settings.tsx
+++ src/settings/Settings.tsx
@@ -5,21 +5,27 @@ import React from "react";
 type SettingsProps = {
     gameState: GameState,
     onSave: any,
+    onClearSave: any
 }
 
 export class Settings extends React.Component<SettingsProps> {
     render() {
-        const {onSave} = this.props;
+        const {onSave, onClearSave} = this.props;
         return (
             <div>
-                <button onClick={() => onSave()}>Save</button>
-                <ThemeContext.Consumer>
-                    {({theme, toggleTheme}) => (
-                        <button onClick={() => toggleTheme()}>
-                            Toggle Dark Mode
-                        </button>
-                    )}
-                </ThemeContext.Consumer>
+                <div>
+                    <button onClick={() => onSave()}>Save</button>
+                    <button onClick={() => onClearSave()}>Clear Save</button>
+                </div>
+                <div>
+                    <ThemeContext.Consumer>
+                        {({theme, toggleTheme}) => (
+                            <button onClick={() => toggleTheme()}>
+                                Toggle Dark Mode
+                            </button>
+                        )}
+                    </ThemeContext.Consumer>
+                </div>
                 <p>Last Saved at ${this.props.gameState.saveTime ? this.props.gameState.saveTime.toString() : ""}</p>
             </div>
         );
